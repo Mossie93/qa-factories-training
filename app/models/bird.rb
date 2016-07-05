482186a57e6bd3717d81bd43af58e191825ca0d6
Rafal Mossakowski <rmossakowski@gmail.com>
2016-07-06 00:13:49 +0200

    Add validations to the bird model

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -2,4 +2,8 @@ class Bird < ActiveRecord::Base
   has_many :user_photos
   has_and_belongs_to_many :regions
   has_and_belongs_to_many :environmental_laws
+
+  validates :name, presence: true, uniqueness: true
+  validates :latin_name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
