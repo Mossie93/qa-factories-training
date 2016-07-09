c845d44c555bbae9c5f00a1ddc4f241b8d8414cd
Rafal Mossakowski <rmossakowski@gmail.com>
2016-07-10 01:57:35 +0200

    Require ffaker gem in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
+require 'ffaker'
 require 'spec_helper'
 require 'rspec/rails'
 require 'site_prism'
